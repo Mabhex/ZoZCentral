422018c9d4514bf7250ca52a771d05b09b00ba09
Bheki <Bheki.Mhlongo@standardbank.co.za>
2023-12-23 19:33:18 +0200

    I hope this works one day

---
+++ TalkWhatsApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace TalkWhatsApp
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            try
+            {
+                string pageId = "101144246045386"; // Replace with your actual page ID
+                string accessToken = "EAAcuBJcuFfcBO9aLFTGQeWtnhf5duJvAGtKIK4kbHgnVVXaC7AQWZAA0Qy2zyWxZCFWXNqocwmUOZCmpsZAW44iQDX5GUVpIODAZCyJY8dZAoV1ZB4gZBECRZBoT1ZAcZCjpZAbkgOxxbuSAJdDkDkK62NuqeuRh2rdXZAoqulzE6d688HYYR6eChMZCBHeiOOsozDM812ytMLCZBeFBiubfejOXJbvcbq475jjBGfSW2jkGqI9HLgZD";
+
+                var httpClient = new HttpClient();
+                var jsonAsString = "{\r\n    \"messaging_product\": \"whatsapp\",\r\n    \"to\": \"27794228401\",\r\n    \"type\": \"template\",\r\n    \"template\": {\r\n        \"name\": \"hello_world\",\r\n        \"language\": {\r\n            \"code\": \"en_US\"\r\n        }\r\n    }\r\n}";
+                var messageContent = new StringContent(jsonAsString);
+
+                // Construct the request URL
+                string requestUrl = $"https://graph.facebook.com/v17.0/{pageId}/messages?access_token={accessToken}";
+
+                // Make the POST request
+                var response = await httpClient.PostAsync(requestUrl, messageContent);
+                response.EnsureSuccessStatusCode();
+
+                // Read the response content (optional)
+                string responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Message sent successfully! Response: {responseContent}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
+
+
+
+        ////token  "EAAcuBJcuFfcBO9aLFTGQeWtnhf5duJvAGtKIK4kbHgnVVXaC7AQWZAA0Qy2zyWxZCFWXNqocwmUOZCmpsZAW44iQDX5GUVpIODAZCyJY8dZAoV1ZB4gZBECRZBoT1ZAcZCjpZAbkgOxxbuSAJdDkDkK62NuqeuRh2rdXZAoqulzE6d688HYYR6eChMZCBHeiOOsozDM812ytMLCZBeFBiubfejOXJbvcbq475jjBGfSW2jkGqI9HLgZD"
+        //WhatsAppAPI whatsAppAPI = new WhatsAppAPI();
+        //    whatsAppAPI.token = "EAAcuBJcuFfcBO9aLFTGQeWtnhf5duJvAGtKIK4kbHgnVVXaC7AQWZAA0Qy2zyWxZCFWXNqocwmUOZCmpsZAW44iQDX5GUVpIODAZCyJY8dZAoV1ZB4gZBECRZBoT1ZAcZCjpZAbkgOxxbuSAJdDkDkK62NuqeuRh2rdXZAoqulzE6d688HYYR6eChMZCBHeiOOsozDM812ytMLCZBeFBiubfejOXJbvcbq475jjBGfSW2jkGqI9HLgZD";
+
+        //    dynamic APIResponse = whatsAppAPI.SendMessage(whatsAppAPI);
+        //    Console.WriteLine(APIResponse);
+        //    Console.ReadLine();
---
+++ TalkWhatsApp/WhatsAppAPI.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkWhatsApp
+{
+    internal class WhatsAppAPI
+    {
+        private string _toNumber;
+        private string _waMessage;
+        private string _phoneNumberId;
+        private string _waBusAccId;
+        private string _token;
+        public string toNumber 
+        {
+            get => _toNumber;
+            set
+            {
+                _toNumber = value;
+            }
+        }
+        public string waMessage
+        {
+            get => _waMessage;
+            set
+            {
+                _waMessage = value;
+            }
+        }
+        
+        public string phoneNumberId
+        {
+            get => _phoneNumberId;
+            set 
+            {
+                _phoneNumberId = value;
+            }
+        }
+
+        public string waBusAccId
+        {
+            get => _waBusAccId;
+            set 
+            { 
+                _waBusAccId = value; 
+            }
+
+        }
+        public string token
+        {
+            get => _token;
+            set
+            {
+                _token = value;
+            }
+
+        }
+        public async Task<dynamic> SendMessage(WhatsAppAPI whatsAppAPI) 
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://graph.facebook.com/v17.0/101144246045386/messages");
+
+            request.Headers.Add("Accept", "application/json; charset=UTF-8");
+            request.Headers.Add("x-redlock-auth", whatsAppAPI.token);
+            var jsonAsString = "{\r\n    \"messaging_product\": \"whatsapp\",\r\n    \"to\": \"27794228401\",\r\n    \"type\": \"template\",\r\n    \"template\": {\r\n        \"name\": \"hello_world\",\r\n        \"language\": {\r\n            \"code\": \"en_US\"\r\n        }\r\n    }\r\n}";
+            var jsonContent = new StringContent(jsonAsString, Encoding.UTF8, "application/json");
+            request.Content = jsonContent;
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            string stringResponse = await response.Content.ReadAsStringAsync();
+            dynamic jsonedstringResponse = JsonConvert.DeserializeObject(stringResponse);
+            return jsonedstringResponse;
+
+        } 
+    }
+}
+
